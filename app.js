6231f587961aea71c6d6954d963c098d095c9326
Radisa Bogdanovic <tictac992@gmail.com>
2022-03-05 22:15:37 +0100

    main

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 'use strict'
 const wrapperDiv= document.querySelector('.wrapper')
 let statement=true;
+
    setInterval(function(){
    if(statement){
     for(let i=0; i<8; i++){  
         wrapperDiv.children[i].classList.remove('crvena-boja', 'zelena-boja')
         wrapperDiv.children[i].classList.add('zuta-boja')
-   }
-
-   }
+   }}
        else{
         let newRequest = new XMLHttpRequest()
         newRequest.open('POST', 'https://www.random.org/integers/?num=1&min=0&max=255&col=1&base=10&format=plain&rnd=new', false)
@@ -23,14 +22,13 @@ let statement=true;
        if(binary[i]==='0'){
            wrapperDiv.children[i].classList.remove('crvena-boja', 'zuta-boja')
            wrapperDiv.children[i].classList.add('zelena-boja')
-        
        }
        if(binary[i]==='1'){
            wrapperDiv.children[i].classList.remove('zelena-boja', 'zuta-boja')
            wrapperDiv.children[i].classList.add('crvena-boja')
-       }
-    }     
-       }
+       } } }
+
     statement=!statement   
+
    },500)
 
